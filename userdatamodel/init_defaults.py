955444c9c6ec1872fe17794c2cfdef4fbfcbfdb3
philloooo <phillis.tt@gmail.com>
2017-04-10 15:24:04 -0500

    feat(init): initialize cloud providers

--- userdatamodel/init_defaults.py
+++ userdatamodel/init_defaults.py
@@ -1,5 +1,10 @@
 from user import IdentityProvider
 IDENTITY_PROVIDERS = ['google', 'itrust']
+CLOUD_PROVIDERS = [
+        {'name': 'cleversafe', 'service': 'storage'},
+        {'name': 'ceph', 'service': 'storage'},
+        {'name': 'AWS', 'service': 'general'}
+]
 
 
 def init_defaults(db):
@@ -10,4 +15,11 @@ def init_defaults(db):
                     .filter(IdentityProvider.name == provider)
                     .first()):
                 provider = IdentityProvider(name=provider)
+                s.add(provider)
+        for provider in CLOUD_PROVIDERS:
+            if not (
+                    s.query(CloudProvider)
+                    .filter(CloudProvider.name == provider['name'])
+                    .first()):
+                provider = CloudProvider(**provider)
                 s.add(provider)
